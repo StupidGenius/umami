b3dd108df1a1d9e52d00dfafe1472dcbb9e4306a
Mark Rogaski <mrogaski@pobox.com>
2015-01-08 04:55:39 +0000

    Added MemberCreationForm.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,69 @@
+"""
+
+The MIT License (MIT)
+
+Copyright (c) 2011-2015 Mark Rogaski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import Member
+
+
+class MemberCreationForm(forms.ModelForm):
+    username = forms.RegexField(label='Username', max_length=30,
+        regex=r'^[A-Za-z0-9_.+-]+$', help_text="Required. 30 characters or fewer. Letters, digits and ./+/-/_ only.",
+        error_messages={'invalid': "This value may contain only letters, numbers and ./+/-/_ characters."}
+    )
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Member
+        fields = ('username',)
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            Member.objects.get(username=username)
+        except Member.DoesNotExist:
+            return username
+        raise forms.ValidationError("A user with that username already exists.")
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(MemberCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
